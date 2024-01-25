5853cc5023f7c92daa5533374c6b7116d52d5b21
Andrew Vos <andrew.vos@gmail.com>
2024-01-25 16:04:40 +0000

    PiP view

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
 
-const chatDelay = 10000
+const chatDelay = 12000
 
 type Line = {
     delay: number,
@@ -113,9 +113,9 @@ export default function Page() {
 
     return (
         <div className="font-serif p-3  h-full flex flex-col space-y-2" >
-            <div className="grid grid-cols-2 gap-2 auto-rows-8">
+            <div className="">
                 <div className="flex justify-center items-center border bg-gray-900">
-                    <div className="relative ">
+                    <div className="relative">
                         <div className="absolute inline bottom-2 left-2 px-1 bg-gray-700 opacity-80 rounded text-white  text-sm font-bold">
                             Andrew Vos
                         </div>
@@ -123,15 +123,13 @@ export default function Page() {
                             {playingMyVideo && (<source src="/videos/me.webm" type="video/webm" />)}
                             {playingMyVideo && (<source src="/videos/me.mp4" type="video/mp4" />)}
                         </video>
-                    </div>
-                </div>
-                <div className="flex justify-center items-center border bg-gray-900">
-                    <div className="relative">
-                        <div className="absolute bottom-2 left-2 px-1 bg-gray-700 opacity-80 rounded text-white  text-sm font-bold">
-                            You
+                        <div className="absolute bottom-0 right-0">
+                            <div className="absolute bottom-2 left-2 px-1 bg-gray-700 opacity-80 rounded text-white  text-sm font-bold">
+                                You
+                            </div>
+                            <video id="videoElement" className="max-w-[120px]" controls={false} autoPlay={true} muted playsInline >
+                            </video>
                         </div>
-                        <video id="videoElement" controls={false} autoPlay={true} muted playsInline >
-                        </video>
                     </div>
                 </div>
             </div>

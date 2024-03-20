30b0ead687377545650e385063aacdf2b555ce40
Andrew Vos <andrew.vos@gmail.com>
2024-03-20 19:11:29 +0000

    Fix images not loading in pdf

--- scripts/build-cv.js
+++ scripts/build-cv.js
@@ -10,6 +10,12 @@ const puppeteer = require("puppeteer");
     ],
   });
   const page = await browser.newPage();
+
+  // https://stackoverflow.com/a/61278565
+  page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36 WAIT_UNTIL=load"
+  );
+
   await page.goto("http://localhost:3000/bio", {
     waitUntil: "domcontentloaded",
   });
